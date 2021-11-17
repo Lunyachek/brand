e093ab0b4bca660485143e64dbc156c40e11a155
Michael Vlasov <mykhailo.vlasov@raccoongang.com>
2021-11-17 15:06:19 +0000

    feat: [RGOeX-407] add styles for the MFE footer component

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
 export { default as logoHeader } from './logo.svg'
+export { default as footerLogo } from './footerLogo.svg';
+export { default as svgSprite } from './svg-sprite.svg';
+
+export const footerIcons = [
+    {
+        url: '#',
+        title: 'facebook',
+        icon: 'ico-facebook'
+    },
+    {
+        url: '#',
+        title: 'twitter',
+        icon: 'ico-twitter'
+    },
+    {
+        url: '#',
+        title: 'instagram',
+        icon: 'ico-instagram'
+    },
+    {
+        url: '#',
+        title: 'linkedin',
+        icon: 'ico-linkedin'
+    },
+    {
+        url: '#',
+        title: 'youtube',
+        icon: 'ico-youtube'
+    },
+    {
+        url: '#',
+        title: 'odnoklassniki',
+        icon: 'ico-odnoklassniki'
+    },
+    {
+        url: '#',
+        title: 'vimeo',
+        icon: 'ico-vimeo'
+    },
+    {
+        url: '#',
+        title: 'vk',
+        icon: 'ico-vk'
+    },
+    {
+        url: '#',
+        title: 'behance',
+        icon: 'ico-behance'
+    },
+    {
+        url: '#',
+        title: 'google-plus',
+        icon: 'ico-google-plus'
+    },
+];
+
+export const footerLinks = [
+    {
+        href: '/about-us',
+        title: 'About',
+        target_blank: false
+    },
+    {
+        href: '/privacy',
+        title: 'Privacy Policy',
+        target_blank: false
+    },
+    {
+        href: '/tos',
+        title: 'Terms of Service',
+        target_blank: false
+    },
+    {
+        href: '/support/contact_us',
+        title: 'Contact Us',
+        target_blank: false
+    },
+];
+
+export const isFooterDark = true;
